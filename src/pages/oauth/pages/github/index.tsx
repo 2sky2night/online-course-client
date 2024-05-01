780a4ac02dbc25081a4c2e868cd177fad19bf7e3
Kinght <1301680879@qq.com>
2024-05-01 22:38:46 +0800

    feat: Github第三方登录

--- src/pages/oauth/pages/github/index.tsx
+++ src/pages/oauth/pages/github/index.tsx
@@ -1,3 +1,22 @@
+import { LoadingPage } from "../../components";
+import { GithubFilled as GithubIcon } from "@ant-design/icons";
+import { authUserControllerGithubLogin as githubLogin } from "@/servers/go_study_server/authUser";
+import { useOAuthLogin } from "../../hooks";
+
 export default function GithubPage() {
-  return <div>github</div>;
+  useOAuthLogin("code", code => {
+    return githubLogin({ code }).then(res => res.data.access_token);
+  });
+  return (
+    <LoadingPage
+      platform="Github"
+      PlatformIcon={() => (
+        <GithubIcon
+          style={{
+            fontSize: "100px",
+          }}
+        />
+      )}
+    />
+  );
 }
