4562d8442918c7fae6cab0b0d3fc5bca77044881
Kinght <1301680879@qq.com>
2024-05-02 10:30:08 +0800

    fix: 调整路由表

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -5,37 +5,40 @@ import GiteePage from "@/pages/oauth/pages/gitee";
 import GithubPage from "@/pages/oauth/pages/github";
 import type { RouteItem } from "./types";
 import NotFoundPage from "@/pages/error/404";
-import { Page } from "@/enums";
 
-// TODO 去除Page的引用，因为route的path不允许出现绝对路径
 export const routes: RouteItem[] = [
   {
-    path: Page.Index,
+    path: "",
     element: <Index />,
     meta: {
       title: "首页",
     },
   },
   {
-    path: Page.OAuthAlipay,
-    element: <AlipayPage />,
-    meta: {
-      title: "支付宝登录",
-    },
-  },
-  {
-    path: Page.OAuthGitee,
-    element: <GiteePage />,
-    meta: {
-      title: "Gitee登录",
-    },
-  },
-  {
-    path: Page.OAuthGithub,
-    element: <GithubPage />,
-    meta: {
-      title: "Github登录",
-    },
+    path: "/oauth",
+    children: [
+      {
+        path: "alipay",
+        element: <AlipayPage />,
+        meta: {
+          title: "支付宝登录",
+        },
+      },
+      {
+        path: "gitee",
+        element: <GiteePage />,
+        meta: {
+          title: "Gitee登录",
+        },
+      },
+      {
+        path: "github",
+        element: <GithubPage />,
+        meta: {
+          title: "Github登录",
+        },
+      },
+    ],
   },
   {
     path: "*",
