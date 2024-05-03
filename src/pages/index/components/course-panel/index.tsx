01b9135877b770b89d549de445aaf67aae078a0d
Kinght <1301680879@qq.com>
2024-05-03 08:29:42 +0800

    feat(index): 分区加载效果

--- src/pages/index/components/course-panel/index.tsx
+++ src/pages/index/components/course-panel/index.tsx
@@ -1,5 +1,6 @@
 // 课程展示 （分区展示）
 import { RightOutlined as RightIcon } from "@ant-design/icons";
+import { Spin } from "antd";
 import { useAntdToken } from "antd-style";
 import { useEffect, useState } from "react";
 
@@ -9,6 +10,7 @@ import type { Partition } from "@/types";
 
 export function CoursePanel() {
   const [list, setList] = useState<Partition[]>([]);
+  const [loading, setLoading] = useState(true);
   const { colorTextDescription } = useAntdToken();
   /** 点击某个分区的回调 */
   const handleGoInfo = () => {
@@ -19,24 +21,31 @@ export function CoursePanel() {
     console.log("查看更多");
   };
   // TODO 是否折叠？
-  // TODO 加载效果
   useEffect(() => {
     partitonList({ limit: 14, offset: 0, desc: false }).then(res => {
       setList(res?.data?.list || []);
+      setLoading(false);
     });
   }, []);
   return (
     <div className="py-3">
       <div className="text-2xl mb-5">🔥热门课程</div>
-      <div className="grid grid-cols-4 gap-2 md:grid-cols-7 mb-5">
-        {list.map(item => (
-          <Button
-            key={item.partition_id}
-            onClick={handleGoInfo}>
-            {item.partition_name}
-          </Button>
-        ))}
-      </div>
+      {loading ? (
+        <div className="flex justify-center">
+          <span className="mr-2">正在加载...</span>
+          <Spin />
+        </div>
+      ) : (
+        <div className="grid grid-cols-4 gap-2 md:grid-cols-7 mb-5">
+          {list.map(item => (
+            <Button
+              key={item.partition_id}
+              onClick={handleGoInfo}>
+              {item.partition_name}
+            </Button>
+          ))}
+        </div>
+      )}
       <div
         className="flex justify-end items-center"
         style={{ color: colorTextDescription }}>
