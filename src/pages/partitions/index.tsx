c8bf84c3a793726e350214a32987d3facdab6bf4
Kinght <1301680879@qq.com>
2024-05-05 17:29:34 +0800

    feat(partition): 全部分区（课程）页

--- src/pages/partitions/index.tsx
+++ src/pages/partitions/index.tsx
@@ -1,5 +1,19 @@
 // 分区列表
+import { PartitionList } from "@/components";
+import { videoPartitionControllerList as partitionList } from "@/servers/go_study_server/videoPartition";
 
 export default function Partitions() {
-  return <div>课程列表</div>;
+  return (
+    <div>
+      <div className="text-3xl my-5">📙全部课程</div>
+      <PartitionList
+        request={(offset, limit) => {
+          return partitionList({ offset, limit, desc: false }).then(res => ({
+            list: res.data.list || [],
+            hasMore: res.data.has_more || false,
+          }));
+        }}
+      />
+    </div>
+  );
 }
