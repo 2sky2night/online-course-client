ebcc1fccf2d474e58e71a4e30c0b9885ac2c1ad9
Kinght <1301680879@qq.com>
2024-05-06 16:06:33 +0800

    fix: 修复登录框样式

--- src/components/login-form/styles.ts
+++ src/components/login-form/styles.ts
@@ -8,7 +8,7 @@ export const useLoginFormStyles = createStyles(({ token, isDarkMode }) => {
       marginBottom: "10px",
     },
     inputEmail: {
-      height: "40px",
+      height: "45px",
       fontSize: token.fontSize,
       outline: "none",
       borderBottom: isDarkMode ? undefined : "1px solid",
@@ -19,13 +19,18 @@ export const useLoginFormStyles = createStyles(({ token, isDarkMode }) => {
       color: token.colorText,
       paddingLeft: "5px",
       paddingRight: "5px",
+      boxSizing: "border-box",
     },
     inputCodeBox: {
       display: "flex",
+      height: "45px",
       borderBottom: isDarkMode ? undefined : "1px solid",
       borderColor: isDarkMode ? undefined : token.colorBorder,
       paddingBottom: "3px",
+      paddingRight: "5px",
       alignItems: "center",
+      background: isDarkMode ? token.colorBgTextActive : undefined,
+      boxSizing: "border-box",
     },
     inputCode: {
       height: "40px",
@@ -35,7 +40,7 @@ export const useLoginFormStyles = createStyles(({ token, isDarkMode }) => {
       paddingLeft: "5px",
       paddingRight: "5px",
       color: token.colorText,
-      background: isDarkMode ? token.colorBgTextActive : undefined,
+      background: "transparent",
     },
     icon: {
       fontSize: "35px",
